97db60aa1e84bf1aa673564282dc33466db8a1e5
ilyes <ilyesarous9645@gmail.com>
2023-09-19 11:48:32 +0100

    first version

---
+++ src/Controller/ProjetController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProjetController extends AbstractController
+{
+    #[Route('/projet', name: 'app_projet')]
+    public function index(): Response
+    {
+        $nom = "ilyes";
+        $class = "3A9";
+        return $this->render('projet/index.html.twig', [
+            'classe' => $class,
+            'nom' => $nom
+        ]);
+    }
+    #[Route('/affiche/{name}/{description}', name:"app_afficher")]
+    public function afficher($name, $description): Response{
+        $projects = [
+            [
+                "name" => "ilyes",
+                "description" => "Studient",
+            ],
+            [
+                "name" => "ahmed",
+                "description" => "Studient 1",
+            ],
+        ];
+        return $this->render('projet/afficher.html.twig', [
+            "name" => $name,
+            "description" => $description,
+            "projects" => $projects,
+        ]);
+    }
+}
